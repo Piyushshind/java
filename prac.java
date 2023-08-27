d0997e9c653e43efc52c2788f3f2036601d77e87
Piyushshind <piyushshinde.199@gmail.com>
2023-08-27 10:40:22 +0530

    java practice

---
+++ prac.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class prac {
+    public static void main(String[] args) {
+        System.out.println("helow world");
+        Scanner sc = new Scanner(System.in);
+        int no1 = sc.nextInt();
+        Scanner sp = new Scanner(System.in);
+        int no2 = sp.nextInt();
+        int sum = no1 + no2;
+        System.out.println(sum);
+
+    }
+}
