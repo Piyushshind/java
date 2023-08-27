0c65bba48db3d51a9e2cc83a712f699f34881aa2
Piyushshind <piyushshinde.199@gmail.com>
2023-08-27 13:49:00 +0530

    changes

---
+++ Practice/Patterns/hollowRectangle.java
@@ -0,0 +1,18 @@
+package Practice.Patterns;
+
+public class hollowRectangle {
+    public static void main(String[] args) {
+        int n = 5, m = 6;
+        for (int i = 1; i <= n; i++) {
+            // inner loop
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 || j == 1 || i == n || j == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
