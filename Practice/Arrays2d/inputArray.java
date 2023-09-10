e205dd0f9c3f6cc2ed0f454b722fdcd6eedbb4bc
Piyushshind <piyushshinde.199@gmail.com>
2023-09-10 09:28:39 +0530

    java

--- Practice/Arrays2d/inputArray.java
+++ Practice/Arrays2d/inputArray.java
@@ -1,5 +1,26 @@
 package Practice.Arrays2d;
 
+import java.util.Scanner;
+
 public class inputArray {
-    
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+
+        int[][] numbers = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+         for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
