0f098f93f9ffe2afac35cac252b13907e28e1b5e
Piyushshind <piyushshinde.199@gmail.com>
2023-09-17 18:27:23 +0530

    j

--- Practice/sum.java
+++ Practice/sum.java
@@ -7,7 +7,7 @@ public class sum {
         //System.out.println("helow world");
         Scanner sc = new Scanner(System.in);
         int no1 = sc.nextInt();
-        //   Scanner sp = new Scanner(System.in);
+        // Scanner sp = new Scanner(System.in);
         int no2 = sc.nextInt();
         int sum = no1 + no2;
         System.out.println(sum);
