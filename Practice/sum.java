dc65322648ade58d14d1ffa279b4aec894850a2a
Piyushshind <piyushshinde.199@gmail.com>
2023-11-21 18:02:23 +0530

    j

--- Practice/sum.java
+++ Practice/sum.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class sum {
     public static void main(String[] args) {
-        //  System.out.println("helow world");
+        // System.out.println("helow world");
         Scanner sc = new Scanner(System.in);
         int no1 = sc.nextInt();
         // Scanner sp = new Scanner(System.in);
