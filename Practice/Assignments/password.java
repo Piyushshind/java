70440b6c6aabce46dd52f77d8ba1a40f23180bd6
Piyushshind <piyushshinde.199@gmail.com>
2024-07-08 08:30:20 +0530

    practice

--- Practice/Assignments/password.java
+++ Practice/Assignments/password.java
@@ -2,28 +2,30 @@ import java.util.Scanner;
 
 public class password {
     public static void main(String[] args) {
+        int a, b;
+
         Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
 
-        String DefuserName = "Piyush07";
-        System.out.println("Enter Username: ");
-        String userName = sc.nextLine();
-        String Defpass = "12345";
-        System.out.println("Enter password: ");
-        String pass = sc.nextLine(); 
+        // String DefuserName = "Piyush07";
+        // System.out.println("Enter Username: ");
+        // String userName = sc.nextLine();
+        // String Defpass = "12345";
+        // System.out.println("Enter password: ");
+        // String pass = sc.nextLine();
 
-        if (DefuserName.equals(userName)) {
-            if (!Defpass.equals(pass)) {
-                System.out.println("Invalid password");
-            } else {
-                System.out.println("Login successful");
-            }
-        } else {
-            System.out.println("Invalid username");
-        }
+        // if (DefuserName.equals(userName)) {
+        // if (!Defpass.equals(pass)) {
+        // System.out.println("Invalid password");
+        // } else {
+        // System.out.println("Login successful");
+        // }
+        // } else {
+        // System.out.println("Invalid username");
+        // }
 
-        sc.close();
+        // sc.close();
     }
 }
-
-
-
